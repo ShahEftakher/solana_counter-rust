7ce518090fcd6aa11a024fda517e48e740b27ffe
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-19 16:04:14 +0600

    Completed on chain rust code

--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -18,21 +18,30 @@ pub struct CounterAccount {
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id: &Pubkey, //Referenced to Public key of the account program is loaded into
+    program_id: &Pubkey, //Referenced to Public key of the program program account
     accounts: &[AccountInfo], //All the account info to be passed with to the chain
     _instruction_data: &[u8] //The instruction to be sent to the chain/program
 ) -> ProgramResult {
     //afaik it is supposed to be the return type of the data to be returned from this function
     let accounts_iter = &mut accounts.iter();
-    msg!("{:?}", accounts_iter);
+
     let account = next_account_info(accounts_iter)?;
-    msg!("{:?}", account);
 
-    msg!("{:?}", program_id);
-
+    //account owner means the
     if account.owner != program_id {
         msg!("Greeted account does not have the correct program id");
         return Err(ProgramError::IncorrectProgramId);
     }
+
+    //creating a instance of the PDA
+    //accessing the variable to update
+    //deserialize this instance from a slice of bytes
+    let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
+    counter_account.counter += 1;
+    //storing on the chain I suppose
+    counter_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("Counter state: {}", counter_account.counter);
+
     Ok(())
 }
